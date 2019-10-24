8546a9c31c5d8a0890c983a1180daa313c71514e
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-24 12:10:13 +0900

    added edit function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show,:edit,:update,:destory]
   def index
     @tasks = Task.all
   end
@@ -21,11 +22,25 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path,notice:"タスクを編集しました"
+    else
+      render :edit
+    end
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name,:content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
