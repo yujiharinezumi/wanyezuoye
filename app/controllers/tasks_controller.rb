84142143c91a2aedb0b2c62c4266537230a5a5f9
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-24 11:46:02 +0900

    added create function and added some gem in gemfile

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name,:content)
+  end
+end
