ec29de9f6359fac4002a6f92cf323e55be03069e
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-24 16:22:24 +0900

    wrote three test code

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -25,26 +25,33 @@ RSpec.feature "タスク管理機能", type: :feature do
  # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
  # タスクのタイトルと内容をそれぞれfill_in（入力）する
  # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-  fill_in "name",with: 'a'
+  fill_in "Name",with: 'a'
  # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-  fill_in "content",with: 'a'
+  fill_in "Content",with: 'a'
  # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
  # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
-  click_button '登録する'
+  click_button 'Create Task'
  # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
  # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
  # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
 
-    save_and_open_page
-
   expect(page).to have_content 'a'
 
   expect(page).to have_content 'a'
-
-
   end
 
   scenario "タスク詳細のテスト" do
+
+    task1 = Task.create!(name: 'test_task_01', content: 'testtesttest')
+    task2 = Task.create!(name: 'test_task_02', content: 'samplesample')
+
+    visit task_path(task1)
+
+
+    expect(page).to have_content 'testtesttest'
+
+
+
 
   end
 end
