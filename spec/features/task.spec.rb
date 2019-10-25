6bd7d20773ee569647f752d0544d34756ee20d01
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-25 12:26:28 +0900

    modified some files

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -6,11 +6,8 @@ RSpec.feature "タスク管理機能", type: :feature do
   scenario "タスク一覧のテスト" do
     Task.create!(name: 'test_task_01', content: 'testtesttest')
     Task.create!(name: 'test_task_02', content: 'samplesample')
-
-
     # tasks_pathにvisitする（タスク一覧ページに遷移する）
     visit tasks_path
-
     # visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
     # have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
     expect(page).to have_content 'testtesttest'
@@ -18,8 +15,7 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク作成のテスト" do
-    # new_task_pathにvisitする（タスク登録ページに遷移する）
-
+ # new_task_pathにvisitする（タスク登録ページに遷移する）
  # 1.ここにnew_task_pathにvisitする処理を書く
    visit new_task_path
  # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
@@ -34,24 +30,17 @@ RSpec.feature "タスク管理機能", type: :feature do
  # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
  # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
  # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-
   expect(page).to have_content 'a'
-
   expect(page).to have_content 'a'
   end
 
   scenario "タスク詳細のテスト" do
-
+  #task1,2の変数を用意しTaskをインスタンス化します。
     task1 = Task.create!(name: 'test_task_01', content: 'testtesttest')
     task2 = Task.create!(name: 'test_task_02', content: 'samplesample')
-
+  #taskの詳細に戦死するように　visitに遷移するようにさせる
     visit task_path(task1)
-
-
+  #該当のタスクが表示されたページのデータが have_contentされているか期待するコードを記述する
     expect(page).to have_content 'testtesttest'
-
-
-
-
   end
 end
