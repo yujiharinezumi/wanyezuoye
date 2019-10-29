9fe8a08e1737158b2221f829b701fedf33e85c04
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-29 12:14:51 +0900

    modified model spec validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
   it "titleが空ならバリデーションが通らない" do
-    task = Task.new(title: '', content: '失敗テスト')
+    task = Task.new(name: '', content: '失敗テスト')
     expect(task).not_to be_valid
   end
 
   it "contentが空ならバリデーションが通らない" do
-    task = Task.new(title: '失敗テスト', content: '')
+    task = Task.new(name: '失敗テスト', content: '')
     expect(task).not_to be_valid
   end
 
   it "titleとcontentに内容が記載されていればバリデーションが通る" do
-    task = Task.new(title: '', content: '')
-    expect(task).not_to_be be_valid
+    task = Task.new(name: '', content: '')
+    expect(task).not_to be_valid
   end
 end
