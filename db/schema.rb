ffcef697a0296d3176d283ae49dfa2d54cf8fc28
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-29 14:10:20 +0900

    first init

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_125844) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "content", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
