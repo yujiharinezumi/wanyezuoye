fa0559620fe22d2011902f41e22761493f971542
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-30 12:25:22 +0900

    finished model/spec/task.test

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :content,presence: true
+    validates :deadline,presence: true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,17 +3,23 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
   it "titleが空ならバリデーションが通らない" do
-    task = Task.new(name: '', content: '失敗テスト')
+    task = Task.new(name: '', content: '失敗テスト',deadline:"12")
     expect(task).not_to be_valid
   end
 
   it "contentが空ならバリデーションが通らない" do
-    task = Task.new(name: '失敗テスト', content: '')
+    task = Task.new(name: '失敗テスト', content: '',deadline:"12")
+    expect(task).not_to be_valid
+  end
+
+
+  it "deadlineが空ならバリデーションが通らない" do
+    task = Task.new(name: '失敗テスト', content: '失敗テスト',deadline:'')
     expect(task).not_to be_valid
   end
 
   it "titleとcontentに内容が記載されていればバリデーションが通る" do
-    task = Task.new(name: '', content: '')
+    task = Task.new(name: '', content: '',deadline:'')
     expect(task).not_to be_valid
   end
 end
