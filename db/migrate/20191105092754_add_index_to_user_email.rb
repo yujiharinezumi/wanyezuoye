cfcd7720b4f1721b7d005d8720fa861d2dca7276
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-11-09 23:39:29 +0900

    modified migration file

--- db/migrate/20191105092754_add_index_to_user_email.rb
+++ db/migrate/20191105092754_add_index_to_user_email.rb
@@ -1,9 +1,5 @@
 class AddIndexToUserEmail < ActiveRecord::Migration[5.2]
   def change
      add_index :users, :email, unique: true
-
-
-      before_validation { email.downcase! }
-      has_secure_password
   end
 end
